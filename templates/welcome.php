02048dcde05c8bc61f2b631e2cc17de30fe99159
Tomasz Plewka <tomasz.plewka66@gmail.com>
2020-12-13 20:00:54 -0700

    added filter, search, add new quiz screens + added styles to inputs, buttons & select form elements

--- templates/welcome.php
+++ templates/welcome.php
@@ -49,6 +49,56 @@
             </div>
         </div>
     </section>
+    <!-- Add new quiz screen -->
+    <section class="background-smoky-black is-flex is-flex-direction-column is-justify-content-start p-5 is-relative hidden-options" id="add-new-quiz-wrapper">
+        <div class="is-flex is-justify-content-space-between">
+            <a id="add-new-quiz-back-btn" class="control-btn">
+                <span class="">
+                    Go Back
+                </span>
+            </a>
+        </div>
+        <div id="add-quiz-form-wrapper" class="columns is-mobile m-0 has-text-centered is-multiline">
+            <div class="column is-12-mobile is-12 p-0">
+                <form action="">
+                    <div class="columns is-mobile m-0 is-vcentered is-multiline">
+                        <div class="column is-12-mobile is-12 p-0 has-text-centered my-2">
+                            <input class="input py-1" type="text" name="quiz-name" placeholder="Name">
+                        </div>
+                        <div class="column is-12-mobile is-12 p-0 has-text-centered my-2">
+                            <div class="select text-ghost-white">
+                                <select class="py-1">
+                                    <option>Quiz type</option>
+                                    <option>With options</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="column is-12-mobile is-12 p-0 has-text-centered my-2 number-input-wrapper">
+                            <div class="number-input">
+                                <button class="decrement"></button>
+                                <input id="quiz-options" class="quantity input mx-3" name="quiz-options" min="2" max="6" step="1" value="2" type="number">
+                                <button class="increment plus"></button>
+                            </div>
+                        </div>
+                        <div class="column is-12-mobile is-12 p-0 has-text-centered mt-2 mb-5 number-input-wrapper">
+                            <div class="number-input">
+                                <button class="decrement"></button>
+                                <input id="quiz-questions" class="quantity input mx-3" name="quiz-options" min="4" max="10" step="1" value="4" type="number">
+                                <button class="increment plus"></button>
+                            </div>
+                        </div>
+                        <div class="column is-12-mobile is-12 p-0">
+                            <button type="submit" name="quiz-generate" id="" class="btn btn-invert m-0">
+                                <span>
+                                    generate
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
     <!-- Browse screen -->
     <section class="background-smoky-black is-flex is-flex-direction-column is-justify-content-start p-5 is-relative hidden-options" id="browse-wrapper">
         <div class="is-flex is-justify-content-space-between">
@@ -70,6 +120,108 @@
                 </a>
             </div>
         </div>
+        <div id="more-info-wrapper" class="scaleY background-ghost-white columns is-mobile m-0 has-text-centered p-5 is-multiline">
+            <div class="is-flex is-justify-content-start column is-12-mobile is-12 p-0">
+                <a id="more-info-back-btn" class="control-btn">
+                    <span class="text-smoky-black">
+                        Go Back
+                    </span>
+                </a>
+            </div>
+            <div class="swiped-right column is-1-mobile is-1 p-0 is-flex is-flex-direction-column is-justify-content-center is-align-items-center text-smoky-black">
+                <i class="fas fa-caret-left"></i>
+            </div>
+            <div class="column is-10-mobile is-10 p-0">
+                <div class="more-info-quiz-info columns is-mobile m-0 is-vcentered is-multiline is-flex is-flex-direction-column is-justify-content-center">
+                    <div class="column is-12-mobile is-12 p-0">
+                        <a id="" class="m-0">
+                            <span>
+                                HERE DISPLAY INFO ABOUT THE QUIZ: NAME, TYPE (CATEGORY), AVERAGE SCORE, LAST ACTIVITY
+                            </span>
+                        </a>
+                    </div>
+                    <div class="column is-12-mobile is-12 p-0">
+                        <a id="" class="btn m-0">
+                            <span>
+                                play
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiped-left column is-1-mobile is-1 p-0 is-flex is-flex-direction-column is-justify-content-center is-align-items-center text-smoky-black">
+                <i class="fas fa-caret-right"></i>
+            </div>
+        </div>
+        <div id="filter-quiz-wrapper" class="scaleY background-ghost-white p-5 ">
+            <div class="columns is-mobile m-0 is-vcentered is-multiline is-flex is-flex-direction-column is-justify-content-start">
+                <div class="is-flex is-justify-content-start column is-12-mobile is-12 p-0">
+                    <a id="filter-quiz-back-btn" class="control-btn">
+                        <span class="text-smoky-black">
+                            Go Back
+                        </span>
+                    </a>
+                </div>
+                <div class="column is-12-mobile is-12 p-0 has-text-centered">
+                    <h2 class="is-size-1 form-header text-smoky-black">FILTER</h2>
+                </div>
+                <div class="column is-12-mobile is-12 p-0">
+                    <form action="">
+                        <div class="columns is-mobile m-0 is-vcentered is-multiline">
+                            <div class="column is-12-mobile is-12 p-0 has-text-centered my-2">
+                                <label class="filter-label" for="category">Quiz category</label>
+                                <div class="select">
+                                    <select>
+                                        <option>Select dropdown</option>
+                                        <option>With options</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="column is-6-mobile is-6 p-0">
+                                <button type="submit" name="filter-clear" id="" class="btn btn-invert btn-small btn-vertical m-0">
+                                    <span>
+                                        clear all
+                                    </span>
+                                </button>
+                            </div>
+                            <div class="column is-6-mobile is-6 p-0">
+                                <button type="submit" name="filter-apply" id="" class="btn btn-invert btn-small btn-vertical m-0">
+                                    <span>
+                                        apply
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div id="search-quiz-wrapper" class="scaleY background-ghost-white p-5 ">
+            <div class="columns is-mobile m-0 is-vcentered is-multiline is-flex is-flex-direction-column is-justify-content-start">
+                <div class="is-flex is-justify-content-start column is-12-mobile is-12 p-0">
+                    <a id="search-quiz-back-btn" class="control-btn">
+                        <span class="text-smoky-black">
+                            Go Back
+                        </span>
+                    </a>
+                </div>
+                <div class="column is-12-mobile is-12 p-0 has-text-centered">
+                    <h2 class="is-size-1 form-header text-smoky-black">SEARCH</h2>
+                </div>
+                <div class="column is-12-mobile is-12 p-0">
+                    <form action="">
+                        <div class="columns is-mobile m-0 is-vcentered is-multiline">
+                            <div class="column is-12-mobile is-12 p-0 has-text-centered my-2">
+                                <input class="input" type="search" name="search" placeholder="Type here">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="column is-12-mobile is-12 p-0">
+                    HERE SHOW LOADER WHILE SEARCHING AND WHEN IT'S DONE SHOW HOW MANY RESULTS
+                </div>
+            </div>
+        </div>
         <div class="quiz-display-wrapper is-flex is-flex-direction-column is-justify-content-start">
             <div class="quiz text-ghost-white my-4">
                 <div class="quiz-header acronym columns is-mobile m-0 is-vcentered has-text-centered py-2">
@@ -84,7 +236,7 @@
                 </div>
                 <div class="quiz-body columns is-mobile m-0 is-vcentered has-text-centered p-0">
                     <div class="quiz-body-more-info column is-6-mobile is-6 p-0">
-                        <a id="" class="btn btn-invert btn-small btn-vertical m-0">
+                        <a id="" class="btn btn-invert btn-small btn-vertical m-0 more-info-btn">
                             <span>
                                 more info
                             </span>
@@ -165,24 +317,7 @@
                     </div> -->
         </div>
         <!-- <div id="test" style="width: 100%;">scroll to understand</div> -->
-        <!-- <div id="content"> </div> -->
-        <div class="quiz-info-wrapper background-smoky-black p-5 hidden-options" id="quiz-info-wrapper">
-            <div class="is-flex is-justify-content-start">
-                <a id="quiz-info-back-btn" class="control-btn">
-                    <span class="">
-                        Go Back
-                    </span>
-                </a>
-            </div>
-            <div class="quiz-info-header">
-                <h2 class="is-size-1 form-header">DETAILS</h2>
-            </div>
-            <div class="quiz-info-body mb-5">
-
-            </div>
-        </div>
-        <!-- <h2 class="is-size-1 form-header">LOG IN</h2> -->
-        
+        <!-- <div id="content"> </div> -->       
     </section>
     <!-- Options -->
     <section class="section background-ghost-white is-flex is-flex-direction-column is-justify-content-center p-5 hidden-options" id="options-wrapper-welcome">
@@ -375,5 +510,135 @@
             setTimeout(() => {
                 welcomeWrapper.firstElementChild.classList.remove('shrink');
             }, 600);
+        });
+        // 
+        document.addEventListener('touchstart', handleTouchStart, false);        
+        document.addEventListener('touchmove', handleTouchMove, false);
+
+        var xDown = null;                                                        
+        var yDown = null;
+
+        function getTouches(evt) {
+            return evt.touches
+        }                                                     
+
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches(evt)[0];                                      
+            xDown = firstTouch.clientX;                                      
+            yDown = firstTouch.clientY;                                      
+        };                                                
+
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+
+            var xUp = evt.touches[0].clientX;                                    
+            var yUp = evt.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    /* left swipe */ 
+                    console.log('swiped left');
+                } else {
+                    /* right swipe */
+                    console.log('swiped right');
+                }                       
+            } else {
+                if ( yDiff > 0 ) {
+                    /* up swipe */ 
+                    console.log('swiped up');
+                } else { 
+                    /* down swipe */
+                    console.log('swiped down');
+                }                                                                 
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;                                             
+        };
+        // 
+        const moreInfoBtn = document.querySelector('.more-info-btn');
+        const moreInfoWrapper = document.querySelector('#more-info-wrapper');
+        // 
+        moreInfoBtn.addEventListener('click', () => {
+            moreInfoWrapper.classList.remove('scaleY');
+        });
+        // 
+        const moreInfoBackBtn = document.querySelector('#more-info-back-btn');
+        // 
+        moreInfoBackBtn.addEventListener('click', () => {
+            moreInfoWrapper.classList.add('scaleY');
+        });
+        // 
+        const filterQuizBtn = document.querySelector('#filter-back-btn');
+        const filterQuizWrapper = document.querySelector('#filter-quiz-wrapper');
+        const filterQuizBackBtn = document.querySelector('#filter-quiz-back-btn');
+        const searchQuizBtn = document.querySelector('#search-back-btn');
+        const searchQuizWrapper = document.querySelector('#search-quiz-wrapper');
+        const searchQuizBackBtn = document.querySelector('#search-quiz-back-btn');
+        // 
+        filterQuizBtn.addEventListener('click', () => {
+            filterQuizWrapper.classList.remove('scaleY');
+        });
+        filterQuizBackBtn.addEventListener('click', () => {
+            filterQuizWrapper.classList.add('scaleY');
+        });
+        // 
+        searchQuizBtn.addEventListener('click', () => {
+            searchQuizWrapper.classList.remove('scaleY');
+        });
+        searchQuizBackBtn.addEventListener('click', () => {
+            searchQuizWrapper.classList.add('scaleY');
+        });
+        // 
+        const addQuizWrapper = document.querySelector('#add-new-quiz-wrapper');
+        const addQuizBackBtn = document.querySelector('#add-new-quiz-back-btn');
+        // 
+        addQuizBtn.addEventListener('click', () => {
+            welcomeWrapper.firstElementChild.classList.add('shrink');
+            // 
+            setTimeout(() => {
+                welcomeWrapper.classList.toggle('hidden-options');
+                addQuizWrapper.classList.toggle('hidden-options');
+            }, 600);
+        });
+        addQuizBackBtn.addEventListener('click', () => {
+            welcomeWrapper.classList.toggle('hidden-options');
+            addQuizWrapper.classList.toggle('hidden-options');
+            // 
+            setTimeout(() => {
+                welcomeWrapper.firstElementChild.classList.remove('shrink');
+            }, 600);
+        });
+        // 
+        const increment = document.querySelectorAll('.increment');
+        const decrement = document.querySelectorAll('.decrement');
+        // const quizOptions = document.querySelector('#quiz-options');
+        // 
+        increment.forEach(incrBtn => {
+            console.log(incrBtn);
+            incrBtn.addEventListener('click', e => {
+                console.log(e.target);
+                if (!(e.target.previousElementSibling.value >= e.target.previousElementSibling.max)) {
+                    e.target.previousElementSibling.value ++;
+                }
+                // 
+                e.preventDefault();
+            });
+        });
+        decrement.forEach(decrBtn => {
+            console.log(decrBtn);
+            decrBtn.addEventListener('click', e => {
+                console.log(e.target);
+                if (e.target.nextElementSibling.value > e.target.nextElementSibling.min) {
+                    e.target.nextElementSibling.value --;
+                }
+                // 
+                e.preventDefault();
+            });
         });
     </script>
